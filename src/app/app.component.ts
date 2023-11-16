1f9565530c1a795b1495518c2358d6d36db719c6
itsgomes <gomesr333@gmail.com>
2023-11-16 14:44:49 -0300

    Studies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Signal, WritableSignal, computed, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,30 +7,103 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div>
+      <p>Counter is: {{counter()}}</p>
+      <button type="button" (click)="increaseCounter()">Increase</button>
+      <button type="button" (click)="decreaseCounter()">Decrease</button>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
+    <hr>
+    <div>
+      <p>Counter computed is: {{counterComputed()}}</p>
+    </div>
+    <hr>
+    <div>
+      <p>Array is: {{arr() | json}}</p>
+      <button type="button" (click)="increaseArray()">Increase</button>
+      <button type="button" (click)="decreaseArray()">Decrease</button>
+    </div>
+    <hr>
+    <div>
+      <p>Object is: {{obj() | json}}</p>
+      <button type="button" (click)="modifyObject()">Modify</button>
+    </div>
+    <hr>
+    <div>
+      <p>Object computed is: {{objComputed() | json}}</p>
+    </div>
   `,
   styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'angular-signals';
+  public counter: WritableSignal<number> = signal(1);
+  public counterComputed: Signal<number> = computed(() => this.counter() * 2);
+  
+  public arr: WritableSignal<number[]> = signal([1, 10, 20]);
+
+  public obj: WritableSignal<{ name: string }[]> = signal(this.generateRandomArray());
+  public objComputed: Signal<string[]> = computed(() => this.obj().map(a => a.name));
+
+  public increaseCounter(): void {
+    /*
+    É possível adicionar um valor utilizando set, como também, o update. A diferença, é que o update concede o valor anterior.
+    Exemplo:
+    this.counter.update(valorAnterior = > valorAnterior + 1);
+    */
+    this.counter.set(this.counter() + 1);
+  }
+
+  public decreaseCounter(): void {
+    this.counter.set(this.counter() - 1);
+  }
+
+  public increaseArray(): void {
+    /*
+    Sempre que é feito uma alteração no valor, essa alteração passa por uma validação chamada equal.
+    Quando usamos um array/objeto, a validação sempre retornará falso.
+    Por isso, é indicado o uso do mutate para fazer um bypass nesse método.
+    */
+    this.arr.mutate(v => v.push(this.generateRandomNumeric()));
+  }
+
+  public decreaseArray(): void {
+    this.arr.mutate(v => v.pop());
+  }
+
+  public modifyObject(): void {
+    this.obj.mutate((currentArray) => {
+      if (currentArray.length > 0) {
+        for (let item of currentArray)
+          item.name = `Item ${this.generateRandomNumeric()}`
+      }
+    });
+  }
+
+  private generateRandomArray(items: number = 3): Array<{ name: string }> {
+    return new Array(items).fill(null).map((_) => ({
+      name: `Item ${this.generateRandomNumeric()}`,
+    }));
+  }
+
+  private generateRandomNumeric(max: number = 100): number {
+    return Math.floor(Math.random() * max);
+  }
+
+  /*
+  Para implementar o effect você pode fazer por meio do construtor da classe, atribuindo a um campo ou passando um injector.
+  Exemplo:
+
+  constructor(private injector: Injector) {}
+    
+  initializeLogging(): void {
+    effect(() => {
+      console.log(`The count is: ${this.count()})`);
+    }, {injector: this.injector});
+  }
+
+  Observação: Não é necessário destruir o effect, ele automaticamente destrói quando o contexto deixa de existir.
+  */
+  private logginEffect = effect(() => {
+    console.log(`Array is ${this.arr()} and the counter is ${this.counter()}`);
+  })
 }
